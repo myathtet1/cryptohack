2f19d39bb31a82d46ce302d80e5c07d6aad75b51
Nhat <nhat.dang@hcmut.edu.vn>
2023-03-16 16:21:04 +0700

    transparency

---
+++ DATA FORMATS/Transparency/main.py
@@ -0,0 +1,7 @@
+import requests, json, sys
+target = sys.argv[1].rstrip()
+
+req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
+json_data = json.loads(req.text)
+for (key,value) in enumerate(json_data):
+    print(value['name_value'])
